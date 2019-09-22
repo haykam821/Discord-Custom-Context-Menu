fa86108cbd95a843632b5c5dc929ea92247389b0
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-09-22 12:06:51 -0400

    Set up Webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	entry: "./src/index.jsx",
+	mode: process.env.WEBPACK_MODE || "production",
+	module: {
+		rules: [{
+			test: /\.jsx$/,
+			use: "jsx-loader",
+		}],
+	},
+	output: {
+		filename: "CustomContextMenu.plugin.js",
+		path: __dirname + "/dist",
+	},
+};
